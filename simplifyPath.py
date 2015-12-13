89b8e6a02687f5787335a25e791ba9bc7dd8bc4f
ylnnyu <ly783@nyu.edu>
2015-12-12 23:57:19 -0800

    add code

---
+++ simplifyPath.py
@@ -0,0 +1,22 @@
+class Solution:
+    # @param path, a string
+    # @return a string
+    def simplifyPath(self, path):
+        stack = []
+        i = 0
+        res = ''
+        while i < len(path):
+            end = i+1
+            while end < len(path) and path[end] != "/":
+                end += 1
+            sub=path[i+1:end]
+            if len(sub) > 0:
+                if sub == "..":
+                    if stack != []: stack.pop()
+                elif sub != ".":
+                    stack.append(sub)
+            i = end
+        if stack == []: return "/"
+        for i in stack:
+            res += "/"+i
+        return res
