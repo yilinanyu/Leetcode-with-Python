2a1d1e1c8dd2ffe5923664bb5bb6aed3f41c28dd
ylnnyu <ly783@nyu.edu>
2015-12-13 22:52:20 -0800

    add code

---
+++ longestPalindromicSubstring.py
@@ -0,0 +1,14 @@
+class Solution:
+    # @return a string
+    def getlongestpalindrome(self, s, l, r):
+        while l >= 0 and r < len(s) and s[l] == s[r]:
+            l -= 1; r += 1
+        return s[l+1 : r]
+    def longestPalindrome(self, s):
+        palindrome = ''
+        for i in range(len(s)):
+            len1 = len(self.getlongestpalindrome(s, i, i))
+            if len1 > len(palindrome): palindrome = self.getlongestpalindrome(s, i, i)
+            len2 = len(self.getlongestpalindrome(s, i, i + 1))
+            if len2 > len(palindrome): palindrome = self.getlongestpalindrome(s, i, i + 1)
+        return palindrome
