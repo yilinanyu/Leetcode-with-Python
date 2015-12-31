ecf1ab154d6cf798e3070b7e33c37ac627452e66
ylnnyu <ly783@nyu.edu>
2015-12-30 23:40:33 -0800

    add code

---
+++ flattenBinaryTreetoLinkedList.py
@@ -0,0 +1,20 @@
+# Definition for a  binary tree node
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    # @param root, a tree node
+    # @return nothing, do it in place
+    def flatten(self, root):
+        # eliminate each level's root's left child
+        while root:
+            if root.left:
+                p = root.left
+                while p.right: p = p.right
+                p.right = root.right
+                root.right = root.left
+                root.left = None
+            root = root.right
