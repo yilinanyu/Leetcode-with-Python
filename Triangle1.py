9936cc065525ff20871f81f7a69ed176c167e4ad
ylnnyu <ly783@nyu.edu>
2015-12-30 23:40:46 -0800

    add code

---
+++ Triangle1.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def minimumTotal(self, triangle):
+        """
+        :type triangle: List[List[int]]
+        :rtype: int
+        """
+        res = [10 ** 10] * len(triangle) # infinity
+        res[0] = triangle[0][0]
+        for i in xrange(1, len(triangle)):
+            for j in xrange(i, 0, -1):
+                res[j] = min(res[j], res[j - 1]) + triangle[i][j]
+            res[0] += triangle[i][0]
+        return min(res)
