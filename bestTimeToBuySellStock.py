aefb81c8d6597a4ef5434fa523824c511bfebc00
ylnnyu <ly783@nyu.edu>
2015-12-31 23:46:10 -0800

    add code

---
+++ bestTimeToBuySellStock.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        minPrice, res = sys.maxint, 0
+        for p in prices:
+            if p < minPrice:
+                minPrice = p
+            if p - minPrice > res:
+                res = p - minPrice
+        return res
