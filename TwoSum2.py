881eeb541d6a9b227cc8579ac72abfdd6ee655b1
ylnnyu <ly783@nyu.edu>
2016-01-12 11:22:31 -0800

    add code

---
+++ TwoSum2.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def twoSum(self, numbers, target):
+        """
+        :type numbers: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        # two-pointer
+        l, r = 0, len(numbers)-1
+        while l < r:
+            s = numbers[l] + numbers[r]
+            if s == target:
+                return [l+1, r+1]
+            elif s < target:
+                l += 1
+            else:
+                r -= 1
+        
