217a8385189e827028882595f91729e004243b70
ylnnyu <ly783@nyu.edu>
2016-01-14 14:42:16 -0800

    add code

---
+++ Search2DMatrix.py
@@ -0,0 +1,19 @@
+class Solution:
+    """
+    @param matrix, a list of lists of integers
+    @param target, an integer
+    @return a boolean, indicate whether matrix contains target
+    """
+    def searchMatrix(self, matrix, target):
+        # write your code here
+        i = 0
+        j = len(matrix[0])-1
+        while i < len(matrix) and j >= 0:
+            if matrix[i][j] == target:
+                return True
+            elif matrix[i][j] > target:
+                j -=1
+            else:
+                i +=1
+        return False
+                
