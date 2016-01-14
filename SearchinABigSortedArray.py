e7a8ab0f36ba738fec9ba2c42a2cdb738e16b561
ylnnyu <ly783@nyu.edu>
2016-01-14 15:35:20 -0800

    add comment

--- SearchinABigSortedArray.py
+++ SearchinABigSortedArray.py
@@ -2,6 +2,15 @@ class Solution:
     # @param {int[]} A an integer array
     # @param {int} target an integer
     # @return {int} an integer
+    ##二分查找
+# 由于有重复数字时，返回第一个出现的index，所以A[mid] == target时，end = mid
+# 由于数组非常大，所以我们希望用一种方式找到刚刚好比target大一些的数作为end，代码如下：
+# end = 0
+# while end < len(A) and A[end] < target:
+#   end = end * 2 + 1
+# if end >= len(A):
+#   end = len(A) - 1
+  #
     def searchBigSortedArray(self, A, target):
         if not A:
             return -1
