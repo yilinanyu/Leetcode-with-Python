565146f58bb7e7f2ae5f95a8df5e30b372361f5f
ylnnyu <ly783@nyu.edu>
2016-01-14 14:42:47 -0800

    add comment

--- Search2DMatrix.py
+++ Search2DMatrix.py
@@ -6,6 +6,7 @@ class Solution:
     """
     def searchMatrix(self, matrix, target):
         # write your code here
+        # 每次总从右侧扫
         i = 0
         j = len(matrix[0])-1
         while i < len(matrix) and j >= 0:
