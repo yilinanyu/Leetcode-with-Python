a163cfbfe7d526c42562768a0f496a0a13d908c8
ylnnyu <ly783@nyu.edu>
2016-01-15 09:58:10 -0800

    v2

--- SearchInBigSortedArray.py
+++ SearchInBigSortedArray.py
@@ -1,21 +1,27 @@
-
+"""
+Definition of ArrayReader:
+class ArrayReader:
+    def get(self, index):
+        # this would return the number on the given index
+        # if there is no number on that index, return -1
+"""
 class Solution:
     # @param {ArrayReader} reader: An instance of ArrayReader 
     # @param {int} target an integer
     # @return {int} an integer
     def searchBigSortedArray(self, reader, target):
+        # write your code here
         index = 0
         while reader.get(index) != -1 and reader.get(index) < target:
-            index = index * 2 + 1
-        
-        start, end = 0, index
+            index = index *2 + 1
+        start = 0
+        end = index
         while start + 1 < end:
-            mid = (start + end) / 2
-            if reader.get(mid) == - 1 or reader.get(mid) >= target:
+            mid = start + (end - start)/2
+            if reader.get(mid)>= target:
                 end = mid
             else:
                 start = mid
-        
         if reader.get(start) == target:
             return start
         if reader.get(end) == target:
