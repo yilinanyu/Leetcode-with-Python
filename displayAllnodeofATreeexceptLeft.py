f0f9679386d52d168a15716beedc4313ccba51aa
ylnnyu <ly783@nyu.edu>
2016-01-20 15:31:32 -0800

    add code

---
+++ displayAllnodeofATreeexceptLeft.py
@@ -0,0 +1,14 @@
+class Solution:
+   
+    def displaynodeexceptLeftMost(root):
+        if 
+
+
+    def disPlayLeftNode(root):
+
+        if root is None:
+            print None
+        elif root.left is None:
+            print root
+        else:
+            self.disPlayLeftNode(root.left)
