e97ca908e0bd770cccbf0a1f428a53b6b9996bec
ylnnyu <ly783@nyu.edu>
2016-01-20 18:48:50 -0800

    add code

---
+++ reverseBit.py
@@ -0,0 +1,10 @@
+class Solution:
+    # @param n, an integer
+    # @return an integer
+    def reverseBits(self, n):
+        ans = 0
+        for i in range(32):
+            ans <<= 1
+            ans |= n & 1
+            n >>= 1
+        return ans
