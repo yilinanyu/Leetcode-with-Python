59faee26d8d6cbc0deae3d12e8e9978107897c09
ylnnyu <ly783@nyu.edu>
2016-01-23 19:03:03 -0800

    add code

---
+++ permutations.py
@@ -0,0 +1,11 @@
+class Solution:
+    # @param num, a list of integer
+    # @return a list of lists of integers
+    def permute(self, num):
+        if len(num) == 0: return []
+        if len(num) == 1: return [num]
+        res = []
+        for i in range(len(num)):
+            for j in self.permute(num[:i] + num[i+1:]):
+                res.append([num[i]] + j)
+        return res
