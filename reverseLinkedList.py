19c93cbc4bbe38e5145c186f26433f25a3b58b6e
ylnnyu <ly783@nyu.edu>
2016-01-27 20:17:48 -0800

    add code

---
+++ reverseLinkedList.py
@@ -0,0 +1,25 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    # @param head, a ListNode
+    # @param m, an integer
+    # @param n, an integer
+    # @return a ListNode
+    def reverseBetween(self, head, m, n):
+        if head == None or head.next == None:
+            return head
+        dummy = ListNode(0); dummy.next = head
+        head1 = dummy
+        for i in range(m - 1):
+            head1 = head1.next
+        p = head1.next
+        for i in range(n - m):
+            tmp = head1.next
+            head1.next = p.next
+            p.next = p.next.next
+            head1.next.next = tmp
+        return dummy.next
