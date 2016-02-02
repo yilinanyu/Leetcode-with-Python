b25f554a9f99cc82187f14ceb0cbb798e9290683
ylnnyu <ly783@nyu.edu>
2016-02-01 18:54:22 -0800

    add code

---
+++ RotateArray2.py
@@ -0,0 +1,15 @@
+class Solution:
+    # @param nums, a list of integer
+    # @param k, num of steps
+    # @return nothing, please modify the nums list in-place.
+    def rotate(self, nums, k):
+        n = len(nums)
+        k %= n
+        self.reverse(nums, 0, n - k)
+        self.reverse(nums, n - k, n)
+        self.reverse(nums, 0, n)
+
+    def reverse(self, nums, start, end):
+        for x in range(start, (start + end) / 2):
+            nums[x], nums[start + end - x - 1] = nums[start + end - x - 1], nums[x]
+        
