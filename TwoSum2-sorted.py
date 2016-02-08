c43772574cf0ffa225d2bb5de29b120b88053d34
ylnnyu <ly783@nyu.edu>
2016-02-07 21:45:06 -0800

    add code

---
+++ TwoSum2-sorted.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def twoSum(self, numbers, target):
+        """
+        :type numbers: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        if numbers is None or len(numbers) == 0:
+            return []
+        l = 0
+        r = len(numbers) - 1
+        while l < r:
+            if numbers[l] + numbers[r] == target:
+                return [l + 1, r + 1]
+            elif numbers[l] + numbers[r] < target:
+                l += 1
+            else:
+                r -= 1
+        return []
+        
+        
