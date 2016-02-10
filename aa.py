47110f362d0d559df4564dddccac7789078bd018
ylnnyu <ly783@nyu.edu>
2016-02-09 23:59:55 -0800

    add

---
+++ aa.py
@@ -0,0 +1,20 @@
+class Solution:
+    #@param A a list of integers
+    #@return an integer
+    def trap(self, A):
+        leftmosthigh = [0 for i in range(len(A))]
+        leftmax = 0
+        for i in range(len(A)):
+            if A[i]> leftmax:
+                leftmax = A[i]
+            leftmosthigh[i] = leftmax
+        rightmax= 0
+        sum = 0
+        for i in reversed(range(len(A))):
+            if A[i] > rightmax:
+                rightmax = A[i]
+            if min(leftmosthigh[i],rightmax)-A[i]>0:
+                sum += min(leftmosthigh[i],rightmax)-A[i]
+        return sum
+
+        
