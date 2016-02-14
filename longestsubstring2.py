79507929b896e7b4031753be165a0f9e879963e9
ylnnyu <ly783@nyu.edu>
2016-02-13 20:24:04 -0800

    add code

---
+++ longestsubstring2.py
@@ -0,0 +1,16 @@
+class Solution:
+    # @return an integer
+    def lengthOfLongestSubstring(self, s):
+        start = 0
+        maxlen = 0
+        dict = {}
+        for i in range(len(s)):
+            dict[s[i]] = -1
+        for i in range(len(s)):
+            if dict[s[i]] != -1:
+                while start <= dict[s[i]]:
+                    dict[s[start]] = -1
+                    start += 1
+            if i - start + 1 > maxlen: maxlen = i - start + 1
+            dict[s[i]] = i
+        return maxlen
