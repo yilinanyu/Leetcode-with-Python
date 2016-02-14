87051de3df73f235cd6f028f579281f827fed9c9
ylnnyu <ly783@nyu.edu>
2016-02-13 20:23:57 -0800

    add code

---
+++ longestSubstring.py
@@ -0,0 +1,14 @@
+class Solution:
+    # @return an integer
+    def lengthOfLongestSubstring(self, s):
+        start = 0
+        maxlen = 0
+        hashtable = [-1 for i in range(256)]
+        for i in range(len(s)):
+            if hashtable[ord(s[i])] != -1:
+                while start <= hashtable[ord(s[i])]:
+                    hashtable[ord(s[start])] = -1
+                    start += 1
+            if i - start + 1 > maxlen: maxlen = i - start + 1
+            hashtable[ord(s[i])] = i
+        return maxlen
