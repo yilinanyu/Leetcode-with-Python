b9e07190b1e1a04181144c1e35f5a007a6245d08
ylnnyu <ly783@nyu.edu>
2016-02-15 18:35:39 -0800

    add code

---
+++ frompreandpost.py
@@ -0,0 +1,11 @@
+
+class Solution:
+
+    def buildTree(self, preorder, inorder):
+        if not inorder: return None # inorder is empty
+        root = TreeNode(preorder[0])
+        rootPos = inorder.index(preorder[0])
+        root.left = self.buildTree(preorder[1 : 1 + rootPos], inorder[ : rootPos])
+        root.right = self.buildTree(preorder[rootPos + 1 : ], inorder[rootPos + 1 : ])
+        return root
+        
