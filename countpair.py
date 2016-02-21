c45daac54c34345c7f4078219c1a108c7df087ea
ylnnyu <ly783@nyu.edu>
2016-02-20 23:57:25 -0800

    add code

---
+++ countpair.py
@@ -0,0 +1,9 @@
+class CountPairs(object):
+def countPairs(num, target): # define a nil dictionary
+dict = {}
+count = 0
+for i in xrange(len(num)):
+x = num[i]
+if target-x in dict:
+count +=1 else:
+dict[x]=i return count
