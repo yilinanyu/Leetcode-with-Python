6257dab527f5e68908a55599510245b37e992359
ylnnyu <ly783@nyu.edu>
2016-02-26 00:07:22 -0800

    test

---
+++ dictlyrics.py
@@ -0,0 +1,9 @@
+def favorsong(A):
+	A.split()
+	dict = {}
+	for i in range(len(A)):
+		dict[A[i]] += 1
+	for values in dict[keys]:
+		return [values,':',dict[keys]]
+A = "a a b b chhc c"
+favorsong(A)
