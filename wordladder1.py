2f351e767507a11e6d3d02af24b4c8aa9849bb9c
ylnnyu <ly783@nyu.edu>
2016-02-29 22:29:43 -0800

    add code

---
+++ wordladder1.py
@@ -0,0 +1,22 @@
+class Solution:
+    # @param start, a string
+    # @param end, a string
+    # @param dict, a set of string!!!dict is a set type!!!
+    # @return an integer
+    def ladderLength(self, start, end, dict):
+        dict.add(end)
+        q = []
+        q.append((start, 1))
+        while q:
+            curr = q.pop(0)
+            currword = curr[0]; currlen = curr[1]
+            if currword == end: return currlen
+            for i in range(len(start)):
+                part1 = currword[:i]; part2 = currword[i+1:]
+                for j in 'abcdefghijklmnopqrstuvwxyz':
+                    if currword[i] != j:
+                        nextword = part1 + j + part2
+                        if nextword in dict:
+                            q.append((nextword, currlen+1)); 
+                            dict.remove(nextword)
+        return 0
