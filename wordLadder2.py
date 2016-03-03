8bb16bdfbb6e926249991308492e9d5e7775e7c3
ylnnyu <ly783@nyu.edu>
2016-03-02 23:08:30 -0800

    add code

--- wordLadder2.py
+++ wordLadder2.py
@@ -3,25 +3,39 @@ class Solution:
     # @param end, a string
     # @param dict, a set of string
     # @return a list of lists of string
-    def findLadders(self, start, end, dic):
-        # thanks to https://leetcode.com/discuss/24191/defaultdict-for-traceback-and-easy-writing-lines-python-code
-        dic.add(end)
-        level = set([start])
-        # key is word, value is parent word, e.g. {'hot': set(['hit']), 'cog': set(['log', 'dog'])}
-        # In each level, defaultdict(set) can remove duplicates, first we need to get parent dictionary
-        parents = collections.defaultdict(set)
-        while level and end not in parents:
-            next_level = collections.defaultdict(set)
-            for word in level:
-                for char in 'abcdefghijklmnopqrstuvwxyz':
-                    for i in xrange(len(start)):
-                        childWord = word[:i] + char + word[i+1:]
-                        if childWord in dic and childWord not in parents: next_level[childWord].add(word)
-            level = next_level
-            parents.update(next_level)
-            
-        # then according parent dictionary, build result from end word to start word
-        res = [[end]]
-        while res and res[0][0] != start:
-            res = [[p] + r for r in res for p in parents[r[0]]]
-        return res
+    def findLadders(self, start, end, dict):
+        def buildpath(path, word):
+            if len(prevMap[word])==0:
+                path.append(word); currPath=path[:]
+                currPath.reverse(); result.append(currPath)
+                path.pop();
+                return
+            path.append(word)
+            for iter in prevMap[word]:
+                buildpath(path, iter)
+            path.pop()
+        
+        result=[]
+        prevMap={}
+        length=len(start)
+        for i in dict:
+            prevMap[i]=[]
+        candidates=[set(),set()]; current=0; previous=1
+        candidates[current].add(start)
+        while True:
+            current, previous=previous, current
+            for i in candidates[previous]: dict.remove(i)
+            candidates[current].clear()
+            for word in candidates[previous]:
+                for i in range(length):
+                    part1=word[:i]; part2=word[i+1:]
+                    for j in 'abcdefghijklmnopqrstuvwxyz':
+                        if word[i]!=j:
+                            nextword=part1+j+part2
+                            if nextword in dict:
+                                prevMap[nextword].append(word)
+                                candidates[current].add(nextword)
+            if len(candidates[current])==0: return result
+            if end in candidates[current]: break
+        buildpath([], end)
+        return result
