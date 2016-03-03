8f6ec17cf43cf6bba7e63b734d293793afc97ca0
ylnnyu <ly783@nyu.edu>
2016-03-03 12:57:46 -0800

    bfs

---
+++ BFS solution of Minimum Depthof BST.py
@@ -0,0 +1,30 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def minDepth(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        if root is None:
+            return 0
+        queue = [root]
+        level = 1
+        while queue:
+            size = len(queue)
+            for i in range(size):
+                node = queue.pop(0)
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+                if node.left is None and node.right is None:
+                    return level
+            level += 1
+        
+        
