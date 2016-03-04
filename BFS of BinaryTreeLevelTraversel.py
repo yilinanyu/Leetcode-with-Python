800c9780b0d403a54f371805879fb8a6d09d0a0e
ylnnyu <ly783@nyu.edu>
2016-03-03 20:54:11 -0800

    add code

---
+++ BFS of BinaryTreeLevelTraversel.py
@@ -0,0 +1,32 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def levelOrder(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[List[int]]
+        """
+        if root is None:
+            return []
+        res = []
+        stack = []
+        stack.append(root)
+        while stack:
+            size = len(stack)
+            level = []
+            for i in range(size):
+                node = stack.pop(0)
+                level.append(node.val)
+                if node.left:
+                    stack.append(node.left)
+                if node.right:
+                    stack.append(node.right)
+            res.append(level)
+        return res
+            
+        
