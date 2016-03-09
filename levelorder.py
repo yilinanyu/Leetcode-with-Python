0534ba554a7c87a743df0e765f00c121d4c226bb
ylnnyu <ly783@nyu.edu>
2016-03-08 23:43:22 -0800

    add code

---
+++ levelorder.py
@@ -0,0 +1,30 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+# BFS: O(N)
+class Solution(object):
+    def levelOrder(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[List[int]]
+        """
+        if root is None:
+            return []
+        res = []
+        queue = [root]
+        while queue:
+            level = []
+            size = len(queue)
+            for i in range(size):
+                node = queue.pop(0)
+                level.append(node.val)
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+            res.append(level)
+        return res
+        
