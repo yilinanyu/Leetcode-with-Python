9b566fe3ea1dadafb5e1a9159cd7bad79efdf849
ylnnyu <ly783@nyu.edu>
2016-03-23 23:41:52 -0700

    add code

--- Lowestcommonancester.py
+++ Lowestcommonancester.py
@@ -1,6 +1,3 @@
-
-
-
 class Solution(object):
     def lowestCommonAncestor(self, root, p, q):
         """
--- t2b.py
+++ t2b.py
@@ -1,4 +1,5 @@
-# // # Let's assume the input ternary expression is valid, we can build the tree in preorder manner.
+# //O(N)
+ # Let's assume the input ternary expression is valid, we can build the tree in preorder manner.
 
 # // # Each time we scan two characters, the first character is either ? question mark or : colon,
 # // # the second character holds the value of the tree node. When we see ?, 
@@ -6,17 +7,21 @@
 # // #When we see :, we need to find out the ancestor node that doesn't have a right node, and make the new node as its right child.
 
 def covertToBST(expression):
+	if len(expression) == 0: return None
 	root = TreeNode(expression[0])
+	#save a reference to the root of the tree and return that, it might be even better
+	n = root
 	stack = []
-	for i in range(0, len(expression),2):
+	for i in range(1, len(expression),2):
 		if expression[i] == "?":
-			root.left = TreeNode(expression[i+1])
-			stack.append(root)
-			root = root.left
+			n.left = TreeNode(expression[i+1])
+			stack.append(n)
+			n = n.left
 		elif expression[i] == ":":
-			root = stack.pop()
-			while root.right != None:
-				root = stack.pop()
-			root.right = TreeNode(expression[i+1])
-			root = root.right
+			n = stack.pop()
+			while n.right != None:
+				n = stack.pop()
+			n.right = TreeNode(expression[i+1])
+			stack.append(n)
+			n = n.right
 	return root
