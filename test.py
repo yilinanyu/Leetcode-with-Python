263c866e6e7195496ea1466cb6d518e6ffa92112
ylnnyu <ly783@nyu.edu>
2016-04-07 23:15:46 -0700

    add code

--- test.py
+++ test.py
@@ -1,15 +1,9 @@
-def removeElement(A, elem):
-    # write your code here
-    if len(A) == 0 or A is None:
-        return None
-    for i in range(len(A)):
-        if A[i] != elem:
-            continue
-        else:
-            A.remove(elem)
-            continue
-    return A
-
-A = [1,2,3,45]
-elem = 2
-print removeElement(A, elem)
+for i in range(1,101):
+    if i % 5 == 0 and i % 7 == 0:
+        print "HelloWorld"
+    elif i % 5 == 0:
+        print "Hello"
+    elif i % 7 == 0:
+        print "World"
+    else:
+        print i
